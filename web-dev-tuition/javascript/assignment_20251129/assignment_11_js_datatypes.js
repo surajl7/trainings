9f50e6b9eefb765c048c7cbf35dd9d8e2dd5d40f
Suraj A L <suraj7connect@gmail.com>
2025-12-01 17:43:34 +0530

    code JavaScript

---
+++ web-dev-tuition/javascript/assignment_20251129/assignment_11_js_datatypes.js
@@ -0,0 +1,8 @@
+var a = 40; //holding number
+var b = "Rahul" //holding string
+
+// document.writeln(a)
+// document.writeln(b)
+
+console.log(a)
+console.log(b)
---
+++ web-dev-tuition/javascript/assignment_20251129/assignment_12_js_operators.js
@@ -0,0 +1,101 @@
+// Addition
+var sum=10+20;
+console.log("Sum of numbers 10 and 20: " + sum);
+
+console.log("=".repeat("100"))
+
+// Subtraction
+var sub=20-10;
+console.log("Subtraction of numbers 20 and 10: " + sub);
+
+console.log("=".repeat("100"))
+
+// Multiplication
+var multi=10*20;
+console.log("Multiplication of numbers 10 and 20: " + multi);
+
+console.log("=".repeat("100"))
+
+// Division
+var div=20/10;
+console.log("Division of numbers 20 and 10: " + div);
+
+console.log("=".repeat("100"))
+
+// Modulus (Remainder)
+var mod = 20%10;
+console.log("Modulus of 20 and 10: " + mod);
+
+console.log("=".repeat("100"))
+
+// Increment
+var a = 10;
+console.log("Value of var: " + a)
+console.log("Post increment of 10: " + a++);
+console.log("Value of var: " + a)
+console.log("Pre increment of 10: " + ++a);
+
+console.log("=".repeat("100"))
+
+// Decrement
+var b = 10;
+console.log("Value of var: " + b)
+console.log("Post decrement of 10: " + b--);
+console.log("Value of var: " + b)
+console.log("Pre decrement of 10: " + --b);
+
+console.log("=".repeat("100"))
+
+// Is equal to
+console.log("10==10: " + (10==10))
+console.log(`10=="10": ` + (10=="10"))
+console.log("10==20: " + (10==20))
+
+console.log("=".repeat("100"))
+
+// Identical (equal and of same type)
+console.log("10===10: " + (10===10))
+console.log(`10==="10": ` + (10==="10"))
+console.log("10===20: " + (10===20))
+
+console.log("=".repeat("100"))
+
+// Not equal to
+console.log("10!=10: " + (10!=10))
+console.log(`10!="10": ` + (10!="10"))
+console.log("10!=20: " + (10!=20))
+
+console.log("=".repeat("100"))
+
+// Not identical
+console.log("10!==10: " + (10!==10))
+console.log(`10!=="10": ` + (10!=="10"))
+console.log("10!==20: " + (10!==20))
+
+console.log("=".repeat("100"))
+
+// Greater than
+console.log("10>10: " + (10>10))
+console.log(`10>"10": ` + (10>"10"))
+console.log("20>10: " + (20>10))
+
+console.log("=".repeat("100"))
+
+// Greater than or equal to
+console.log("10>=10: " + (10>=10))
+console.log(`10>="10": ` + (10>="10"))
+console.log("10>=20: " + (10>=20))
+
+console.log("=".repeat("100"))
+
+// less than
+console.log("10<10: " + (10<10))
+console.log(`10<"10": ` + (10<"10"))
+console.log("10<20: " + (10<20))
+
+console.log("=".repeat("100"))
+
+// less than equal to
+console.log("10<=10: " + (10<=10))
+console.log(`10<="10": ` + (10<="10"))
+console.log("10<=20: " + (10<=20))
